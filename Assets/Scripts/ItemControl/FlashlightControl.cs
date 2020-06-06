34a7bb4747d30bc2823c6b19ae46d179c06e9ceb
SimonCalver <simon.calver@pmb.ox.ac.uk>
2020-06-06 21:18:35 +0100

    Added flashlight

---
+++ Assets/Scripts/ItemControl/FlashlightControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class FlashlightControl : MonoBehaviour
+{
+    public float lightIntensity;
+    bool lightOn;
+    UnityEngine.Experimental.Rendering.Universal.Light2D light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = this.gameObject.transform.GetChild(0).GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+        light.intensity = lightIntensity;
+        lightOn = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("space") && lightOn)
+        {
+            light.intensity = 0;
+            lightOn = false;
+        }
+        if (Input.GetKeyDown("space") && !lightOn)
+        {
+            light.intensity = lightIntensity;
+            lightOn = true;
+        } 
+    }
+}
