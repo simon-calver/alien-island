cefb982e9ed2b62e66fc204e3300b12d0adeba51
SimonCalver <simon.calver@pmb.ox.ac.uk>
2020-06-20 10:39:59 +0100

    Basic inventory working

---
+++ Assets/Scripts/Inventory/DynamicInterface.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DynamicInterface : UserInterface
+{
+    public GameObject inventoryPrefab;
+
+    public int x_start;
+    public int y_start;
+
+    public int x_space_between_item;
+    public int number_of_columns;
+    public int y_space_between_items;
+
+    public override void CreateSlots()
+    {
+        slotsOnInterface = new Dictionary<GameObject, InventorySlot>();
+        for (int i = 0; i < inventory.GetSlots.Length; i++)
+        {
+            var obj = Instantiate(inventoryPrefab, Vector3.zero, Quaternion.identity, transform);
+            obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+
+            AddEvent(obj, EventTriggerType.PointerEnter, delegate { OnEnter(obj); });
+            AddEvent(obj, EventTriggerType.PointerExit, delegate { OnExit(obj); });
+            AddEvent(obj, EventTriggerType.BeginDrag, delegate { OnDragStart(obj); });
+            AddEvent(obj, EventTriggerType.EndDrag, delegate { OnDragEnd(obj); });
+            AddEvent(obj, EventTriggerType.Drag, delegate { OnDrag(obj); });
+
+            inventory.GetSlots[i].slotDisplay = obj;
+
+            slotsOnInterface.Add(obj, inventory.GetSlots[i]);
+        }
+    }
+
+    private Vector3 GetPosition(int i)
+    {
+        return new Vector3(x_start + (x_space_between_item * (i % number_of_columns)), y_start + (-y_space_between_items * (i / number_of_columns)), 0f);
+    }
+}
---
+++ Assets/Scripts/Inventory/ItemDatabaseObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item Database", menuName = "Inventory System/Items/Database")]
+public class ItemDatabaseObject : ScriptableObject, ISerializationCallbackReceiver
+{
+    public ItemObject[] ItemObjects;
+    // public Dictionary<int, ItemObject> GetItem = new Dictionary<int, ItemObject>();
+
+    public void UpdateID()
+    {
+        for (int i = 0; i < ItemObjects.Length; i++)
+        {
+            if(ItemObjects[i].data.Id != i)
+                ItemObjects[i].data.Id = i;
+            // GetItem.Add(i, Items[i]);
+        }
+    }
+
+    public void OnAfterDeserialize()
+    {
+        UpdateID();
+    }
+
+    public void OnBeforeSerialize()
+    {
+      //  GetItem = new Dictionary<int, ItemObject>();
+    }
+}
